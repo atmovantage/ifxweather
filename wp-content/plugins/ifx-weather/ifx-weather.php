83acec2bb65cd506163857345b3fda17d1b947f8
owner <owner@austinsatmosphere.com>
2016-07-25 12:00:00 -0400

    added options submenu, Day1 meta field created

--- wp-content/plugins/ifx-weather/ifx-weather.php
+++ wp-content/plugins/ifx-weather/ifx-weather.php
@@ -371,13 +371,7 @@ function run_ifx_weather() {
 
 // add_action( 'admin_menu', 'ifxwx_admin_menu' );
 
-// function ifxwx_admin_menu() {
-// 	add_menu_page( 'iFx Weather', 'iFx Weather', 'manage_options', 'ifxwx', 'ifxwx_admin_page', 'dashicons-cloud', 6  );
-// 	add_submenu_page( 'ifxwx','','Overview','manage_options','ifxwx','ifxwx_admin_page');
-// 	add_submenu_page( 'ifxwx', 'View Forecasts', 'View Forecasts', 'manage_options', 'ifxwx-view-fx.php', 'ifxwx_admin_view_fx' );
-// 	add_submenu_page( 'ifxwx', 'Add Forecast', 'Add Forecast', 'manage_options', 'ifxwx-add-fx.php', 'ifxwx_admin_add_fx' );
-// 	add_submenu_page( 'ifxwx', 'Options', 'Options', 'manage_options', 'ifxwx-options.php', 'ifxwx_admin_options' );
-// }
+
 
 
 
@@ -399,7 +393,7 @@ function forecast_post() {
 		'update_item'           => __( 'Update Forecast', 'text_domain' ),
 		'view_item'             => __( 'View Forecast', 'text_domain' ),
 		'search_items'          => __( 'Search Forecasts', 'text_domain' ),
-		'not_found'             => __( 'Forecast Not found', 'text_domain' ),
+		'not_found'             => __( 'No Forecasts Found', 'text_domain' ),
 		'not_found_in_trash'    => __( 'Not found in Trash', 'text_domain' ),
 		'featured_image'        => __( 'Forecast Feature Image', 'text_domain' ),
 		'set_featured_image'    => __( 'Set forecast featured image', 'text_domain' ),
@@ -415,7 +409,7 @@ function forecast_post() {
 		'label'                 => __( 'Forecast', 'text_domain' ),
 		'description'           => __( 'Weather forecast', 'text_domain' ),
 		'labels'                => $labels,
-		'supports'              => array( 'title', 'editor', 'author', 'thumbnail', 'custom-fields', 'page-attributes', 'post-formats', ),
+		'supports'              => array( 'title', 'editor', 'thumbnail', ),
 		'taxonomies'            => array( 'category', 'post_tag' ),
 		'hierarchical'          => false,
 		'public'                => true,
@@ -430,12 +424,167 @@ function forecast_post() {
 		'publicly_queryable'    => true,
 		'capability_type'       => 'post',
 	);
+	
 	register_post_type( 'forecast', $args );
-
+	
 }
 add_action( 'init', 'forecast_post', 0 );
 
+add_action('admin_menu', 'ifx_weather_submenu');
 
+function ifx_weather_submenu()
+{
+	add_submenu_page( 'edit.php?post_type=forecast', 'Options', 'Options', 'manage_options', 'ifxwx_options', 'ifxwx_admin_options');
+}
+
+class Day1_Meta_Box {
+
+	public function __construct() {
+
+		if ( is_admin() ) {
+			add_action( 'load-post.php',     array( $this, 'init_metabox' ) );
+			add_action( 'load-post-new.php', array( $this, 'init_metabox' ) );
+		}
+
+	}
+
+	public function init_metabox() {
+
+		add_action( 'add_meta_boxes', array( $this, 'add_metabox'  )        );
+		add_action( 'save_post',      array( $this, 'save_metabox' ), 10, 2 );
+
+	}
+
+	public function add_metabox() {
+
+		add_meta_box(
+			'day1_fx',
+			__( 'Day 1 Forecast', 'text_domain' ),
+			array( $this, 'render_metabox' ),
+			'forecast',
+			'advanced',
+			'default'
+		);
+
+	}
+
+	public function render_metabox( $post ) {
+
+		// Add nonce for security and authentication.
+		wp_nonce_field( 'ifxwx_nonce_action', 'ifxwx_nonce' );
+
+		// Retrieve an existing value from the database.
+		$car_year = get_post_meta( $post->ID, 'car_year', true );
+		$car_mileage = get_post_meta( $post->ID, 'car_mileage', true );
+		$car_cruise_control = get_post_meta( $post->ID, 'car_cruise_control', true );
+		$car_power_windows = get_post_meta( $post->ID, 'car_power_windows', true );
+		$car_sunroof = get_post_meta( $post->ID, 'car_sunroof', true );
+
+		// Set default values.
+		if( empty( $car_year ) ) $car_year = '';
+		if( empty( $car_mileage ) ) $car_mileage = '';
+		if( empty( $car_cruise_control ) ) $car_cruise_control = '';
+		if( empty( $car_power_windows ) ) $car_power_windows = '';
+		if( empty( $car_sunroof ) ) $car_sunroof = '';
+
+		// Form fields.
+		echo '<table class="form-table">';
+
+		echo '	<tr>';
+		echo '		<th><label for="car_year" class="car_year_label">' . __( 'Year', 'text_domain' ) . '</label></th>';
+		echo '		<td>';
+		echo '			<input type="text" id="car_year" name="car_year" class="car_year_field" placeholder="' . esc_attr__( '', 'text_domain' ) . '" value="' . esc_attr__( $car_year ) . '">';
+		echo '		</td>';
+		echo '	</tr>';
+
+		echo '	<tr>';
+		echo '		<th><label for="car_mileage" class="car_mileage_label">' . __( 'Mileage', 'text_domain' ) . '</label></th>';
+		echo '		<td>';
+		echo '			<input type="number" id="car_mileage" name="car_mileage" class="car_mileage_field" placeholder="' . esc_attr__( '', 'text_domain' ) . '" value="' . esc_attr__( $car_mileage ) . '">';
+		echo '		</td>';
+		echo '	</tr>';
+
+		echo '	<tr>';
+		echo '		<th><label for="car_cruise_control" class="car_cruise_control_label">' . __( 'Cruise Control', 'text_domain' ) . '</label></th>';
+		echo '		<td>';
+		echo '			<input type="checkbox" id="car_cruise_control" name="car_cruise_control" class="car_cruise_control_field" value="' . $car_cruise_control . '" ' . checked( $car_cruise_control, 'checked', false ) . '> ' . __( '', 'text_domain' );
+		echo '			<span class="description">' . __( 'Car has cruise control.', 'text_domain' ) . '</span>';
+		echo '		</td>';
+		echo '	</tr>';
+
+		echo '	<tr>';
+		echo '		<th><label for="car_power_windows" class="car_power_windows_label">' . __( 'Power Windows', 'text_domain' ) . '</label></th>';
+		echo '		<td>';
+		echo '			<input type="checkbox" id="car_power_windows" name="car_power_windows" class="car_power_windows_field" value="' . $car_power_windows . '" ' . checked( $car_power_windows, 'checked', false ) . '> ' . __( '', 'text_domain' );
+		echo '			<span class="description">' . __( 'Car has power windows.', 'text_domain' ) . '</span>';
+		echo '		</td>';
+		echo '	</tr>';
+
+		echo '	<tr>';
+		echo '		<th><label for="car_sunroof" class="car_sunroof_label">' . __( 'Sunroof', 'text_domain' ) . '</label></th>';
+		echo '		<td>';
+		echo '			<input type="checkbox" id="car_sunroof" name="car_sunroof" class="car_sunroof_field" value="' . $car_sunroof . '" ' . checked( $car_sunroof, 'checked', false ) . '> ' . __( '', 'text_domain' );
+		echo '			<span class="description">' . __( 'Car has sunroof.', 'text_domain' ) . '</span>';
+		echo '		</td>';
+		echo '	</tr>';
+
+		echo '</table>';
+
+	}
+
+	public function save_metabox( $post_id, $post ) {
+
+		// Add nonce for security and authentication.
+		$nonce_name   = $_POST['ifxwx_nonce'];
+		$nonce_action = 'ifxwx_nonce_action';
+
+		// Check if a nonce is set.
+		if ( ! isset( $nonce_name ) )
+			return;
+
+		// Check if a nonce is valid.
+		if ( ! wp_verify_nonce( $nonce_name, $nonce_action ) )
+			return;
+
+		// Check if the user has permissions to save data.
+		if ( ! current_user_can( 'edit_post', $post_id ) )
+			return;
+
+		// Check if it's not an autosave.
+		if ( wp_is_post_autosave( $post_id ) )
+			return;
+
+		// Check if it's not a revision.
+		if ( wp_is_post_revision( $post_id ) )
+			return;
+
+		// Sanitize user input.
+		$car_new_year = isset( $_POST[ 'car_year' ] ) ? sanitize_text_field( $_POST[ 'car_year' ] ) : '';
+		$car_new_mileage = isset( $_POST[ 'car_mileage' ] ) ? sanitize_text_field( $_POST[ 'car_mileage' ] ) : '';
+		$car_new_cruise_control = isset( $_POST[ 'car_cruise_control' ] ) ? 'checked' : '';
+		$car_new_power_windows = isset( $_POST[ 'car_power_windows' ] ) ? 'checked' : '';
+		$car_new_sunroof = isset( $_POST[ 'car_sunroof' ] ) ? 'checked' : '';
+
+		// Update the meta field in the database.
+		update_post_meta( $post_id, 'car_year', $car_new_year );
+		update_post_meta( $post_id, 'car_mileage', $car_new_mileage );
+		update_post_meta( $post_id, 'car_cruise_control', $car_new_cruise_control );
+		update_post_meta( $post_id, 'car_power_windows', $car_new_power_windows );
+		update_post_meta( $post_id, 'car_sunroof', $car_new_sunroof );
+
+	}
+
+}
+
+new Day1_Meta_Box;
+
+// function ifxwx_admin_menu() {
+// 	add_menu_page( 'iFx Weather', 'iFx Weather', 'manage_options', 'ifxwx', 'ifxwx_admin_page', 'dashicons-cloud', 6  );
+// 	add_submenu_page( 'ifxwx','','Overview','manage_options','ifxwx','ifxwx_admin_page');
+// 	add_submenu_page( 'ifxwx', 'View Forecasts', 'View Forecasts', 'manage_options', 'ifxwx-view-fx.php', 'ifxwx_admin_view_fx' );
+// 	add_submenu_page( 'ifxwx', 'Add Forecast', 'Add Forecast', 'manage_options', 'ifxwx-add-fx.php', 'ifxwx_admin_add_fx' );
+
+// }
 
 function ifxwx_admin_page(){
 	?>
